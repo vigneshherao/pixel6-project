1043ccb586af7fe081fd2bf5f907b1738330d42a
vignesh <vigneshfornavy@gmail.com>
2024-07-30 14:58:42 +0530

    Implemented NavBar Component

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import { LOGO, MENU } from '../utils/constants'
 
 const NavBar = () => {
   return (
-    <div>NavBar</div>
+    <div className='bg-white w-[100%] h-20 shadow-md flex justify-between px-5 sm:px-20 items-center cursor-pointer'>
+        <div className='w-10'>
+            <img src={LOGO} alt="logo" />
+            <p className='font-bold'>Pixel<span className='text-red-700 font-bold'>6</span></p>
+        </div>
+        <div className='w-14'>
+            <img src={MENU} alt='menu'/>
+        </div>
+    </div>
   )
 }
 
---
+++ src/utils/constants.js
@@ -0,0 +1,3 @@
+
+export const LOGO = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShwN8RtzBT6EIllyR4Agp_QFobIfDwOtwgSw&s";
+export const MENU = "https://thumb.ac-illust.com/41/41a956254bcd191e942220ca7fb13d71_t.jpeg";

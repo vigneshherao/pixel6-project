da2d5b12c626942d0c4de9c742ecad6e74e6cee0
vignesh <vigneshfornavy@gmail.com>
2024-07-31 15:31:25 +0530

    Conditonal Rendering Shimmer Component

---
+++ src/components/Shimmer.js
@@ -0,0 +1,99 @@
+import React from "react";
+
+const Shimmer = () => {
+  return (
+    <div className="relative overflow-x-auto mx-4 sm:mx-20 p-0 sm:p-10">
+      <table className="min-w-full divide-y divide-gray-200">
+        <tbody className="bg-white divide-y divide-gray-200">
+          <tr className="animate-pulse">
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-48 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-32 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+          </tr>
+          <tr className="animate-pulse">
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-48 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-32 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+          </tr>
+          <tr className="animate-pulse">
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-48 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-32 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+          </tr>
+          <tr className="animate-pulse">
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-48 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-32 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+          </tr>
+          <tr className="animate-pulse">
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-48 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-32 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+            <td className="px-2 py-4 sm:px-6 whitespace-nowrap">
+              <div className="bg-gray-200 h-4 w-24 rounded"></div>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  );
+};
+
+export default Shimmer;
--- src/components/TableBody.js
+++ src/components/TableBody.js
@@ -2,14 +2,21 @@ import DataTable from "react-data-table-component";
 import { COLUMNS } from "../utils/constants";
 import useFetchUsers from "../utils/useFetchUsers";
 import { useSelector } from "react-redux";
+import Shimmer from "./Shimmer";
 
 const TableBody = () => {
-
+  
+  //subscribing the store for getting user Detail from data slice
   const users = useSelector((store) => store?.users?.usersData);
   
+  //Calling the custom hook for the dispatching the data to store
   useFetchUsers();
-   
 
+  //Conditional Rendering if user is null return shimmer
+  if(!users){
+    return <Shimmer/>
+  }
+  
   return (
     <div className="sm:mx-16 mt-10">
          <DataTable className="border rounded-lg border-gray-200"  columns={COLUMNS} data={users} pagination/> 

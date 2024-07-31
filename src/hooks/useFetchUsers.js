e35b03bf0f435e96c03175acd8d479e77911a9de
vignesh <vigneshfornavy@gmail.com>
2024-07-31 15:55:56 +0530

    Added comments to useFectchUser hook

--- src/hooks/useFetchUsers.js
+++ src/hooks/useFetchUsers.js
@@ -1,23 +1,23 @@
-import { useEffect} from "react";
+import { useEffect } from "react";
 import { User_DATA } from "../utils/constants";
 import { useDispatch } from "react-redux";
 import { addUsersData } from "../utils/dataSlice";
 
 const useFetchUsers = () => {
-
+  
   const dispatch = useDispatch();
 
   useEffect(() => {
     userData();
   }, []);
 
+  //function to get the user data from api
   const userData = async () => {
     const data = await fetch(User_DATA);
     const dataJson = await data.json();
+    //dispatching an action to the redux store
     dispatch(addUsersData(dataJson?.users));
   };
 };
-
-
 
 export default useFetchUsers;
